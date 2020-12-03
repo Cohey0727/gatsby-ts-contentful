9f1fe0f03e870b49773efbd1aa9a36b945b05dfd
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-12-04 02:16:54 +0900

    master

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Typescript Starter`,
-    authors: [
-      { name: 'Tori', slug: 'tori' },
-      { name: 'Neko', slug: 'neko' },
-      { name: 'Inu', slug: 'inu' }
-    ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
